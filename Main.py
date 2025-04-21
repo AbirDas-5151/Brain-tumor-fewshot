34651cd8f2c61ec1b1e04879c14e32f70eac3297
AbirDas-5151 <Addyabir111@gmail.com>
2025-04-21 23:19:53 +0900

    Deleted Everything

--- Main.py
+++
@@ -1,7 +0,0 @@
-from data.dataset import load_crystal_clean_data
-from trainer.experiment_runner import run_few_shot_experiments
-from config import device, crystal_path
-
-if __name__ == "__main__":
-    train_loaders, val_loaders = load_crystal_clean_data(crystal_path)
-    run_few_shot_experiments(train_loaders, val_loaders, device)
